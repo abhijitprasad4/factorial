e02aee8908cfded655b356785277481d6ead2873
abhijitprasad4 <94544539+abhijitprasad4@users.noreply.github.com>
2022-01-16 00:46:03 +0530

    Update settings.py

--- factorial/settings.py
+++ factorial/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q2d_*x#d#&y+9p60n!g7)jz))_)eirk*809nphtgly71gh1330'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
